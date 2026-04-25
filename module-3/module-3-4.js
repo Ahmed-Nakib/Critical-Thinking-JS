3c23c91a731d245fd21e2f8937a7aec21cd42db3
Ahmed-Nakib <office.nakib@gmail.com>
2026-04-25 06:52:03 +0600

    module3-4

---
+++ module-3/module-3-4.js
@@ -0,0 +1,46 @@
+//* Array Implementation
+
+
+class Queue {
+    constructor (){
+        this.items = [];
+    }
+
+    //* 0(1)
+    enqueue(value){
+        this.items.push(value)
+    }
+
+    //*O(n)
+    dequeue(){
+        if(this.isEmpty()){
+            return undefined
+        }
+        return this.items.shift()
+    }
+
+    //*O(1)
+    peek(){
+        if(this.isEmpty()){
+            return undefined
+        }
+        return this.items(0)
+    }
+
+    //*O(n)
+    print(){
+        console.log("Start -> ", this.items.join(" -> ", " -> end"));
+        
+    }
+}
+
+const queue = new Queue()
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+
+queue.dequeue()
+queue.print();
