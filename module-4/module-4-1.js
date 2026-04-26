8391ffe8589b2a81aa4ddd63e5b1e146cf421597
Ahmed-Nakib <office.nakib@gmail.com>
2026-04-26 07:54:59 +0600

    module-4-1

---
+++ module-4/module-4-1.js
@@ -0,0 +1,31 @@
+ const dataCache = new Map();
+
+ const expensiveTask = (id) => {
+    console.log("Run the expensive task for: ", id );
+    
+    return {
+        id: id,
+        data: `Some data for id: ${id}`,
+        timestamp : new Date().getTime()
+    }
+    
+ }
+
+
+ const getData = (id) => {
+    if(dataCache.has(id)){
+        console.log("Cache HIT fot id: ", id);
+        
+        return dataCache.get(id)
+    }
+    console.log("Cache MISS fot id: ", id);
+    const data = expensiveTask(id)
+    dataCache.set(id, data)
+    return data;
+ }
+
+ console.log(dataCache);
+ console.log(getData(123));
+ console.log(getData(123));
+ console.log(dataCache);
+ 
