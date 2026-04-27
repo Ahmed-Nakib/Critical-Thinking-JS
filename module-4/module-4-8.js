0fea810dfef24488723a4366e3514de001c4bc93
Ahmed-Nakib <office.nakib@gmail.com>
2026-04-27 15:05:54 +0600

    module-4-8.js

---
+++ module-4/module-4-8.js
@@ -0,0 +1,23 @@
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        console.log("State of arr:", arr);
+
+        let minIndex = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j
+            }
+            
+        }
+
+        if(minIndex !== i){
+            let temp = arr[i]
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        console.log(`After pass ${i + 1}`, arr);
+        
+    }
+}
+
+selectionSort([5, 3, 8, 2])
